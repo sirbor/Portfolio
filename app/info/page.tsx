25f739ae8be0ea7c460f29212586651eab6a7b86
Dominic Bor <dominicbor44@gmail.com>
2024-10-11 18:29:58 +0300

    Updated some of my skills

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -43,9 +43,8 @@ export default async function InfoPage() {
         I've developed a diverse set of technical skills:
         </p>
               <ul>
-          <li><strong>Programming Languages:</strong> Java, Kotlin , Javascript, Typescript, Go</li>
-          <li><strong>Frameworks:</strong> Springboot, Spring MVC, Jetpack Compose, Angular Android UI, Retrofit, Redis, Rest Api</li>
-          <li><strong>Tools and Technologies:</strong> Maven , Gradle, Docker , Postgres, Firebase, Prometheus, Jest, AWS</li>
+          <li><strong>Tools and Technologies:</strong> Java, Kotlin , Javascript , Go , Springboot , Spring MVC , Angular, Jetpack Compose,
+          Android UI , Retrofit , Redis , Rest Api , Maven , Gradle , Docker , Postgres , Firebase , Prometheus , Jest , AWS</li>
         </ul>
 
         <p>
